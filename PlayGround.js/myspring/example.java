7976520addbe9a225a5353bfdec671e10c63236e
Debbie <119403063+Successor5@users.noreply.github.com>
2023-05-17 15:25:42 +0100

    Add files via upload

---
+++ PlayGround.js/myspring/example.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class example {
+    List<String> words = List.of(
+"Aa"
+      "BBB"
+      "QWERTY"
+      "ASDFG"
+    );
+    Map<boolean, Set<String>>map=words.stream().collect(Collectors.())
+
+}
