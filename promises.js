c5cf725b6158fa5607149b19a61c54991e014958
Debbie <119403063+Successor5@users.noreply.github.com>
2023-05-21 13:05:57 +0100

    Create promises.js

---
+++ promises.js
@@ -0,0 +1,20 @@
+// function promiseFunction(){
+//     const myPromise = new Promise((resolve,reject) => {
+//         const data= {name:'john',age:30};
+//         if(data){
+//             resolve(data);
+//         }else{
+//             reject('errr fetching data from Api')
+//         }
+//     });
+//     myPromise
+//     .then(data=>console.log(data))
+//     .catch(error=>console.error(error));
+// }
+// promiseFunction()
+
+// const card = ()=>{
+    
+// }
+// var nam =pr
+//  console.log(nam)
